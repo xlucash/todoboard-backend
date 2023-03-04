89d83119afc28b00e6c69342b61de31d2721f370
xlucash <lukiereq@gmail.com>
2023-03-04 17:48:58 +0100

    Initial commit

---
+++ src/main/java/me/xlucash/todoboard/TodoboardApplication.java
@@ -0,0 +1,13 @@
+package me.xlucash.todoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/xlucash/todoboard/TodoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xlucash.todoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
